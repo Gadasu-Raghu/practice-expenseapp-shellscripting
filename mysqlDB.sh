47585d7bc24da964e5819f658ebae19e8702384e
raghu2671 <raghu.gadasu2017@gmail.com>
2024-12-28 11:52:49 +0530

    mysql.repo content issue

--- mysqlDB.sh
+++ mysqlDB.sh
@@ -1,5 +1,7 @@
 dnf module disable mysql -y
 
+
+
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 
 dnf install mysql-community-server -y
@@ -8,3 +10,5 @@ systemctl enable mysqld
 systemctl start mysqld
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
+
+mysql -uroot -pExpenseApp@1
